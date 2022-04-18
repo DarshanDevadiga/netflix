ba705331cdaa9bb26bc81a7ef2fad881f320819a
Your Name <darshandevadiga00@gmail.com>
2022-04-18 12:09:10 +0530

    make it better

---
+++ js/signin-up.js
@@ -0,0 +1,9 @@
+//SIGN IN
+  document.querySelector(".signin-button").addEventListener("click", e => {
+    e.preventDefault();
+document.location.href="home.html";
+});
+document.querySelector(".signup-button").addEventListener("click", e => {
+    e.preventDefault();
+    document.location.href = "login.html";
+});
